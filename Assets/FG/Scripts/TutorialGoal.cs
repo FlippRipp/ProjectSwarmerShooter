94cf4e3e6b38ff60665667567b977642b5c27b28
Filip <filip@sigrand.se>
2020-09-22 16:58:45 +0200

    Small fixes and a Pre-Built build of the game v1.01

---
+++ Assets/FG/Scripts/TutorialGoal.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using  UnityEngine.SceneManagement;
+
+public class TutorialGoal : MonoBehaviour
+{
+    [SerializeField] private MeshRenderer goal;
+    // Update is called once per frame
+
+    private void Awake()
+    {
+        goal.enabled = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("PowerUp"))
+        {
+            goal.enabled = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(!other.CompareTag("Player")) return;
+        SceneManager.LoadScene("MainMenu");
+    }
+}
